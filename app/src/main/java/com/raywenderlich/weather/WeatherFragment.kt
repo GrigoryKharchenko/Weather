29584d9ea6becb204de7fcaacd69266f02fc902d
GrigoryKharchenko <96061286+GrigoryKharchenko@users.noreply.github.com>
2022-04-19 21:58:04 +0600

    [feature/day1] Create splashScreen,create design and click listener

--- app/src/main/java/com/raywenderlich/weather/WeatherFragment.kt
+++ app/src/main/java/com/raywenderlich/weather/WeatherFragment.kt
@@ -5,23 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainer
+import com.google.android.material.snackbar.Snackbar
 import com.raywenderlich.weather.databinding.FragmentWeatherBinding
 
-class WeatherFragment:Fragment() {
-    private var binding:FragmentWeatherBinding? = null
+class WeatherFragment : Fragment() {
+    private var binding: FragmentWeatherBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState:Bundle?): View?{
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentWeatherBinding.inflate(layoutInflater)
         return binding?.root
     }
 
-    companion object{
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        changeCity()
+        clickDegreesListener()
+        clickLocation()
+    }
+
+    private fun showSnackBar(value: String) {
+        Snackbar.make(requireView(), value, Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun changeCity() {
+        binding?.tvChangeCity?.setOnClickListener {
+            showSnackBar("Сменить город")
+        }
+    }
+
+    private fun clickDegreesListener() {
+        binding?.rbCelsius?.setOnClickListener {
+            showSnackBar("C")
+        }
+        binding?.rbFahrenheit?.setOnClickListener {
+            showSnackBar("F")
+        }
+    }
+
+    private fun clickLocation() {
+        binding?.tvMyLocation?.setOnClickListener {
+            showSnackBar("Мое местополоожение")
+        }
+    }
+
+    companion object {
         const val TAG = "WeatherFragment"
 
         fun newInstance() = WeatherFragment()
     }
-
 }
